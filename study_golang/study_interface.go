1a697e490b7a085475741d941b9b1858e243917b
xyh <975573743@qq.com>
2023-08-23 17:07:09 +0800

    golang

--- study_golang/study_interface.go
+++ study_golang/study_interface.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 //空接口
 //var c interface{}
@@ -26,12 +29,26 @@ func test(h Human) {
 }
 
 func main() {
-	var a Human
-	var b Human
-	t := Tom{}
-	a = t
-	test(a)
-	j := Jim{}
-	b = j
-	test(b)
+	//var a Human
+	//var b Human
+	//t := Tom{}
+	//a = t
+	//test(a)
+	//j := Jim{}
+	//b = j
+	//test(b)
+	names := []string{
+		"3. Triple Kill",
+		"5. Penta Kill",
+		"2. Double Kill",
+		"4. Quadra Kill",
+		"1. First Blood",
+	}
+
+	sort.Strings(names)
+
+	// 遍历打印结果
+	for _, v := range names {
+		fmt.Printf("%s\n", v)
+	}
 }
